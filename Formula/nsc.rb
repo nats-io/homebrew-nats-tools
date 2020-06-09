44bdbd90f48f604c8cfa2a23e57b195eaa54ced3
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-09 12:40:21 -0500

    Brew formula update for nsc version 0.4.10

--- Formula/nsc.rb
+++ Formula/nsc.rb
@@ -2,16 +2,16 @@
 class Nsc < Formula
   desc "A tool for creating NATS account and user access configurations"
   homepage "https://github.com/nats-io/nsc"
-  version "0.4.8"
+  version "0.4.10"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/nats-io/nsc/releases/download/0.4.8/nsc-darwin-amd64.zip"
-    sha256 "7c7f45138a9a3c38f5a44919bb5b277dc84aa683561ed2c75c4dca68255827ec"
+    url "https://github.com/nats-io/nsc/releases/download/0.4.10/nsc-darwin-amd64.zip"
+    sha256 "d72b3d3d198eac7dab62685f06cfd48e0abd24913e10006aa7dd4d1cf792242d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/nats-io/nsc/releases/download/0.4.8/nsc-linux-amd64.zip"
-      sha256 "fe56a04c01ba10b77bddfbc86295666607a6a9351a68f347742e9103fcdef57a"
+      url "https://github.com/nats-io/nsc/releases/download/0.4.10/nsc-linux-amd64.zip"
+      sha256 "aad09716d3d2ae2f8f237edb3f12677820d7b7e08a10ff16dd631f8b845393f0"
     end
   end
 
