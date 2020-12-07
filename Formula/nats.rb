07b295ba57da9ec284db6cf1916db684a79f1b9b
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-07 14:04:28 +0100

    Brew formula update for natscli version 0.0.20

--- Formula/nats.rb
+++ Formula/nats.rb
@@ -7,19 +7,19 @@ class Nats < Formula
 
   if OS.mac?
     url "https://github.com/nats-io/natscli/releases/download/0.0.20/nats-darwin-amd64.zip"
-    sha256 "ee018400e83b1954325b6783b683b13a70d64d1ef4c5925e7a8bc7ee4e6d7b8d"
+    sha256 "cdbdfaf48e1e5217c0f834f9065434eb3e2b97ece3d15099df98119fa837e063"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/nats-io/natscli/releases/download/0.0.20/nats-linux-amd64.zip"
-    sha256 "cc4af188d697e030f3572091256bb7fba2c62d1c0ac52ba98572bb765e2b1369"
+    sha256 "ed831ef34ef1c0f7b61e45a8da4b28c51dcb9006cd8e71752b8648af9654a26d"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/nats-io/natscli/releases/download/0.0.20/nats-linux-arm.zip"
-    sha256 "8c2b07302d885911d7025320176a3f21727696f4b854450376fd6ad261ab4b5e"
+    sha256 "869f20b4355458a5659431ab1825dade21353fe46658ddb4d0b5912b4c9ff814"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/nats-io/natscli/releases/download/0.0.20/nats-linux-arm64.zip"
-    sha256 "121a4037ba2c4960618389b4f4c8813306052bde22c19f0219a46a18e2ae97fe"
+    sha256 "6436facd67ebbda859f5e3966dce83f724e483af60c152da4bc7d408ca6680c3"
   end
 
   def install
