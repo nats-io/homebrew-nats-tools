fcdeece4e80f33dc2daf964559842e4e195e1b58
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-21 15:38:20 -0400

    Brew formula update for nsc version 2.2.4

--- Formula/nsc.rb
+++ Formula/nsc.rb
@@ -5,16 +5,20 @@
 class Nsc < Formula
   desc "A tool for creating NATS account and user access configurations"
   homepage "https://github.com/nats-io/nsc"
-  version "2.2.3"
+  version "2.2.4"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/nats-io/nsc/releases/download/2.2.3/nsc-darwin-amd64.zip"
-    sha256 "d642f75db7d1d3d72d5a7ee5a9f0142c7dce6ac20ce7994af55c2d141b3dd0bc"
+    url "https://github.com/nats-io/nsc/releases/download/2.2.4/nsc-darwin-amd64.zip"
+    sha256 "ad3fa940027b52b3d7b79c40706ef5dbc473212104480ceda659876d5440beb5"
+  end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/nats-io/nsc/releases/download/2.2.4/nsc-darwin-arm64.zip"
+    sha256 "f7972d68fb41c67b28745f5ed6ff6bf04e7f4cd3c939d950eed309a454d3d9e6"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/nats-io/nsc/releases/download/2.2.3/nsc-linux-amd64.zip"
-    sha256 "7acfbb4572e2c3e91f76bad04d28edc951a1d3892d2497862434c0d9b1f00c14"
+    url "https://github.com/nats-io/nsc/releases/download/2.2.4/nsc-linux-amd64.zip"
+    sha256 "65b0f5929b50820d48c0904054bdbe9a3f76c61907b530ec42faad78da40987d"
   end
 
   def install
